c171a2f70ded5c488f449113546d4e37879ddded
Sebastian Schmitt <sebastian.schmitt.konz@gmx.de>
2025-04-12 22:47:39 +0200

    initv2

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gradient-to-br from-pink-100 to-pink-300">
+      <h1 className="text-4xl font-bold text-pink-800">Melina & Fabio 💍</h1>
+    </div>
+  );
+}
+
+export default App;
