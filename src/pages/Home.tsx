dde5ff75b44d6f3d24b5bc87fe7df4af2c250e88
Seppseb <44102028+Seppseb@users.noreply.github.com>
2025-08-18 19:09:32 +0200

    changed password to hash

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,33 +2,48 @@
 import { useEffect, useState } from 'react';
 import WeddingContent from '../components/WeddingContent.js'; // Import the new component
 
+async function sha256(message: string) {
+  const msgBuffer = new TextEncoder().encode(message);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
+
+
 export default function Home() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [isChecking, setIsChecking] = useState(false);
 
-  const correctPassword = "test"; 
+  const correctPasswordHash = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08";
 
-  const handlePasswordSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (password === correctPassword) {
-      localStorage.setItem("wedding_password", password);
+  const checkPassword = async (passwordp : string) => {
+    setIsChecking(true);
+    setError('');
+    const inputHash = await sha256(passwordp);
+    if (inputHash === correctPasswordHash) {
+      localStorage.setItem("wedding_password", passwordp);
       setIsAuthenticated(true);
     } else {
+      localStorage.removeItem("wedding_password");
       setError('Falsches Passwort. Bitte versucht es erneut.');
+      setIsChecking(false);
     }
+  }
+
+  const handlePasswordSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await checkPassword(password);
   };
 
-  useEffect(() => {
+  useEffect( () => {
     // Load saved password on mount
     const savedPassword = localStorage.getItem("wedding_password");
     if (savedPassword) {
       setPassword(savedPassword);
-      if (savedPassword === correctPassword) {
-        setIsAuthenticated(true);
-      } else {
-        localStorage.removeItem("wedding_password");
-      }
+      checkPassword(savedPassword);
     }
   }, []);
 
@@ -48,9 +63,10 @@ export default function Home() {
             />
             <button
               type="submit"
+              disabled={isChecking}
               className="w-full bg-lavender hover:bg-lavender-dark text-white text-lg px-6 py-2 rounded-full transition-colors"
             >
-              Enter
+              {isChecking ? 'Prüfe...' : 'Enter'}
             </button>
             {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
           </form>
@@ -62,3 +78,4 @@ export default function Home() {
   // If authenticated, show the main wedding content
   return <WeddingContent />;
 }
+
